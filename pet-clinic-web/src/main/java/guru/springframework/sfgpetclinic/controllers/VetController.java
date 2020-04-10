25cd9a4ac0a085a6e7f11c94612b65561b399a58
Fabio Abreu <fnmabreu@gmail.com>
2020-04-10 19:30:14 +0100

    Implement Find Owner. Closes #54.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -3,7 +3,7 @@ package guru.springframework.sfgpetclinic.controllers;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VetController {
@@ -14,7 +14,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "vets.html"})
+    @GetMapping({"/vets", "/vets/index", "/vets/index.html", "vets.html"})
     public String listVets(Model model) {
 
         model.addAttribute("vets", vetService.findAll());
